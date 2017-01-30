2457609b4c6b6a79d277b98dc4fa961decc2a476
Junior <nog.junior84@gmail.com>
2017-01-30 19:15:24 -0200

    First Commit

---
+++ desafio-programacao-1b/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DesafioProgramacao1b::Application.config.secret_key_base = '1b6606f3a1aa3694cb19ccca501871925823dee7c5ffd969aac425b5e791d0b73d51c96c5d24c44f5506a173815a9941abb218c9bec03ad12a1b6a91f388d004'
