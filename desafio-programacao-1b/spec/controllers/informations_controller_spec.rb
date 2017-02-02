31eda9eb7d2e9980477ffdbbdad896ba7ad5348d
Junior <nog.junior84@gmail.com>
2017-02-02 12:34:49 -0200

    Specs Controller

--- desafio-programacao-1b/spec/controllers/informations_controller_spec.rb
+++ desafio-programacao-1b/spec/controllers/informations_controller_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe InformationsController, type: :controller do
+  describe "GET #index" do
+    it "populate array informations" do
+      file = FactoryGirl.create(:information)
+      get :index
 
+      expect(assigns(:informations)).to eq([file])
+    end
+
+    it "render the index view" do
+      get :index
+
+      expect(subject).to render_template(:index)
+      expect(subject).to render_template("index")
+    end
+
+    it "does not render a different template" do
+      get :index
+
+      expect(subject).to_not render_template(:show)
+      expect(subject).to_not render_template("show")
+    end
+  end
+
+  describe "GET #show" do
+
+    it 'assigns the requested file to @information' do
+      file = FactoryGirl.create(:information)
+      get :show, id: file
+
+      expect(assigns(:information)).to eq(file)
+    end
+
+    it "render the show view" do
+      get :show, id: FactoryGirl.create(:information)
+
+      expect(subject).to render_template(:show)
+      expect(subject).to render_template("show")
+    end
+
+    it "does not render a different template" do
+      get :show, id: FactoryGirl.create(:information)
+
+      expect(subject).to_not render_template(:create)
+      expect(subject).to_not render_template("create")
+    end
+  end
+
+  describe "POST #create" do
+    context "with a valid attributes" do
+      it "create a new file" do
+        expect{
+          post :create, information: FactoryGirl.attributes_for(:information)
+        }.to change(Information, :count).by(1)
+      end
+
+      it "redirect to a view file" do
+        post :create, information: FactoryGirl.attributes_for(:information)
+
+        expect(response.content_type).to eq "text/html"
+      end
+    end
+
+    context "with a invalid attributes" do
+      it "does not save the new contact" do
+        expect{
+          post :create, information: FactoryGirl.attributes_for(:invalid_information)
+        }.to_not change(Information,:count)
+      end
+
+      it "re-renders the new file" do
+        post :create, information: FactoryGirl.attributes_for(:invalid_information)
+
+        expect(subject).to render_template(:index)
+        expect(subject).to render_template("index")
+      end
+    end
+  end
 end
--- desafio-programacao-1b/spec/factories/information.rb
+++ desafio-programacao-1b/spec/factories/information.rb
@@ -3,5 +3,15 @@ include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :information do
     document { fixture_file_upload(Rails.root.join('spec/fixtures/test_data.txt'), 'text/plain') }
+    document_file_size 8
+    document_file_name "test_data.txt"
+    document_updated_at Time.now
+  end
+
+  factory :invalid_information, parent: :information do |f|
+    f.document { fixture_file_upload(Rails.root.join('spec/fixtures/test_data.pdf'), 'application/pdf') }
+    f.document_file_size 8
+    f.document_file_name "test_data.pdf"
+    f.document_updated_at Time.now
   end
 end
